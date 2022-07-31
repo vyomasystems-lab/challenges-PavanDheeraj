ed3d2d106f453949f1e03c51c7844f5ebbf2cce1
PavanDheeraj <58168687+PavanDheeraj@users.noreply.github.com>
2022-07-31 12:47:03 +0530

    Bugs rectified

--- level1_design2/seq_detect_1011.v
+++ level1_design2/seq_detect_1011.v
@@ -46,7 +46,7 @@ module seq_detect_1011(seq_seen, inp_bit, reset, clk);
       SEQ_1:
       begin
         if(inp_bit == 1)
-          next_state = IDLE;
+          next_state = SEQ_1;
         else
           next_state = SEQ_10;
       end
@@ -62,7 +62,7 @@ module seq_detect_1011(seq_seen, inp_bit, reset, clk);
         if(inp_bit == 1)
           next_state = SEQ_1011;
         else
-          next_state = IDLE;
+          next_state = SEQ_10;
       end
       SEQ_1011:
       begin
